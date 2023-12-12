c988e09762d70b2fc62409bf6023aff2721383e6
Okellothomas <thomasolal33@gmail.com>
2023-12-12 07:15:34 +0300

    add changes

---
+++ app/about/layout.tsx
@@ -0,0 +1,15 @@
+
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+      <>
+          <nav>About navbar</nav>
+          <main>{ children }</main>
+          <footer>About footer</footer>
+      </>
+  )
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function About() {
+    return (
+    <>
+      <div>About</div>
+      <Link href="/"> Link to the main page!</Link>
+    </>
+    
+  )
+}
